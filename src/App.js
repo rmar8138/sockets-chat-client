e05b3bb73f41ba397215b90cad79f88d2de95e58
rmar8138 <ragan.martinez@live.com>
2019-12-21 16:34:24 +1100

    initial commit and basic client side socket setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    endpoint: "http://127.0.0.1:5000"
+  };
+
+  componentDidMount() {
+    // start socket connection
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.emit("message", "hello from the client side!");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
